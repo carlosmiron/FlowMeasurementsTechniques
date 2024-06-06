2a2ea2145576837e5d41493e24c4918aa3519bd0
Jarno11 <jarno.platenburg@gmail.com>
2024-06-06 16:55:18 +0200

    Generalized fft

--- HWA_processing.py
+++ HWA_processing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.fft import rfft, rfftfreq
+
 
 
 ############################################## Functions #################################################
@@ -28,6 +30,12 @@ def avg_voltage(df):
 
 def voltage_to_velocity(coefficients, voltage):
     return np.polyval(coefficients, voltage)
+
+def fourier(signal, Ts):
+    #Compute the fourier transform
+    signal_fft = rfft(signal)
+    freqs = rfftfreq(len(signal), Ts)
+    return signal_fft, freqs
 
 
 ############################################## Calibration #################################################
@@ -187,72 +195,87 @@ if fluc_velocity_plot:
 
 ############################################## Spectral Analysis #################################################
 
-#Compute the fourier tranform of a sine wave using scipy
-from scipy.fft import rfft, rfftfreq
 
-def fourier(signal, Ts):
-    #Compute the fourier transform
-    signal_fft = rfft(signal)
-    freqs = rfftfreq(len(signal), Ts)
-    return signal_fft, freqs
-
-
-#Use one velocity signal as an example
-voltage_example = txt_to_df('HWA/Mesruements_15_+24.txt')
-velocities_example = voltage_to_velocity(polyfit_coefficients, voltage_example['Voltage'].astype(float))
-#Remove nans from the array
-velocities_example = velocities_example[~np.isnan(velocities_example)]
-#Remove average velocity
-fluctuations_example = velocities_example - np.mean(velocities_example)
-
-#Plot the velocity signal
-fig = plt.figure()
-plt.plot(velocities_example)
-plt.ylabel('Velocity')
-plt.title('Velocity Signal')
-plt.show()
-
-#Compute the fourier transform of the velocity signal
-fluctuations_example_fft, freqs = fourier(fluctuations_example, T_sample)
+#Initialize fft_dict as dictionary 
+fft_dict = {}
+freqs_dict = {}
+#Give it keys for each position and angle
+for angle in angles:
+    fft_dict[angle] = {}
+    freqs_dict[angle] = {}
+    for position in positions:
+        fft_dict[angle][position] = []
+        freqs_dict[angle][position] = []
 
 
-#Plot the fourier transform
-fig = plt.figure()
-plt.plot(freqs, np.abs(fluctuations_example_fft))
-plt.xlabel('Frequency [Hz]')
-plt.ylabel('Amplitude')
-plt.title('Fourier Transform of the fluctuations Signal')
-plt.show()
+
+for angle in angles:
+    for position in positions:
+        filename = 'HWA/Mesruements_' + angle + '_' + position + '.txt'
+        df = txt_to_df(filename)
+        mean_voltage = avg_voltage(df)
+        mean_vel = np.polyval(polyfit_coefficients, mean_voltage)
+
+        voltages_array = np.array(df['Voltage'].astype(float))
+        #Remove all instances of nan
+        voltages_array = voltages_array[~np.isnan(voltages_array)]
+        
+        velocities_array = np.polyval(polyfit_coefficients, voltages_array)
+        fluctuations_array = velocities_array - mean_vel
+
+        #Compute the fourier transform
+        fluctuations_fft, freqs = fourier(fluctuations_array, T_sample) 
+
+        if angle == "00":
+            fft_dict[angle][position].append(np.abs(fluctuations_fft))
+            freqs_dict[angle][position].append(freqs)
+        elif angle == "05":
+            fft_dict[angle][position].append(np.abs(fluctuations_fft))
+            freqs_dict[angle][position].append(freqs)
+        elif angle == "15":
+            fft_dict[angle][position].append(np.abs(fluctuations_fft))
+            freqs_dict[angle][position].append(freqs)
 
 
-"""
-#TESTING THE FOURIER TRANSFORM FUNCTION
-#Determine the fourier transform of a simple sine wave
-#Generate a sine wave
-fs = 1000
-t = np.linspace(0, 1, fs)
-f1 = 10
-f2 = 200
-f3 = 500
-sine_wave = np.sin(2 * np.pi * f1 * t) + np.sin(2 * np.pi * f2 * t) + np.sin(2 * np.pi * f3 * t)
-#Compute the fourier transform
-sine_wave_fft, freqs = fourier(sine_wave, 1/fs)
 
-#Plot the sine wave
-fig = plt.figure()
-plt.plot(t, sine_wave)
-plt.ylabel('Amplitude')
-plt.title('Sine Wave')
-plt.show()
+fourier_plot_00 = True
+if fourier_plot_00:     
+    fig = plt.figure()
+    #Plot the fourier transform of angle is 0 and every four positions
+    for i in range(0, len(positions), 4):
+        plt.plot(freqs_dict["00"][positions[i]][0], fft_dict["00"][positions[i]][0], label = positions[i])
 
-#Plot the fourier transform of the sine wave
-fig = plt.figure()
-plt.plot(freqs, np.abs(sine_wave_fft))
-plt.xlabel('Frequency [Hz]')
-plt.ylabel('Amplitude')
-plt.title('Fourier Transform of the Sine Wave')
-plt.show()
-"""
+    plt.legend()
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude')
+    plt.title('Fourier Transform of the Fluctuations Signal')
+    plt.show()
 
 
+fourier_plot_05 = False
+if fourier_plot_05:     
+    fig = plt.figure()
+    #Plot the fourier transform of angle is 0 and every four positions
+    for i in range(0, len(positions), 4):
+        plt.plot(freqs_dict["05"][positions[i]][0], fft_dict["05"][positions[i]][0], label = positions[i])
+
+    plt.legend()
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude')
+    plt.title('Fourier Transform of the Fluctuations Signal')
+    plt.show()
+    
+
+fourier_plot_15 = False
+if fourier_plot_15:     
+    fig = plt.figure()
+    #Plot the fourier transform of angle is 0 and every four positions
+    for i in range(0, len(positions), 4):
+        plt.plot(freqs_dict["15"][positions[i]][0], fft_dict["15"][positions[i]][0], label = positions[i])
+
+    plt.legend()
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Amplitude')
+    plt.title('Fourier Transform of the Fluctuations Signal')
+    plt.show()
 
